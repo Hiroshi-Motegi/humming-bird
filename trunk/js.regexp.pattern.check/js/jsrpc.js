f64494a2cac5ee3ca8d2071f20296bd373b25458
ysnr.ws <ysnr.ws@3b15c62c-4380-11de-9805-9778bc541ee5>
2009-05-25 20:27:54 +0000

    パターンチェック、初回インポート

---
+++ trunk/js.regexp.pattern.check/js/jsrpc.js
@@ -0,0 +1,251 @@
+(function($){
+$.rpc = {
+	//Replace or Match function.
+	fnc: function(){
+		var word = $($.my.slctr.tgtTxtr).val();
+		
+		//Exist target string.
+		if (!word) {
+			$.rpc.showResult("対象文字列を入力して下さい。");
+			return false;
+		}
+		
+		var pattern = $($.my.slctr.ptTxt).val();
+		
+		//valid pattern.
+		if ($.rpc.isRegexPattern(pattern)) {
+			pattern = $.rpc.convertRegex(pattern);
+		}
+		else {
+			$.rpc.showResult("Patternが正しくありません。");
+			return false;
+		}
+		
+		//Exec Replace or Match function.
+		$.rpc.exec[$.rpc.getExecOrder()].call(this, pattern, word);
+		return false;
+	},
+	
+	showResult: function(s){
+		$("#txtr-result").val(s);
+	},
+	getExecOrder: function(){
+		return $($.my.slctr.checkedOrder).val();
+	},
+	exec: {
+		match: function(pt, str){
+			$.rpc.showResult(str.match(pt));
+		},
+		replace: function(pt, str){
+			$.rpc.showResult(str.replace(pt, $($.my.slctr.reTxt).val()));
+		}
+	},
+	isRegexPattern: function(pattern){
+		return pattern.match(/^\/.+\/(?:[img]){0,3}$/);
+	},
+	convertRegex: function(s){
+		var n = s.lastIndexOf('/'), pattern = s.substr(1, n - 1), flag = s.substr(n + 1, s.length);
+		
+		try {
+			return new RegExp(pattern, flag);
+		} 
+		catch (e) {
+			$.rpc.showResult(e.msg + "\nパターンが正しくありません。");
+			return false;
+		}
+	}
+};
+
+/*
+$.fn.setData = function(key, val){
+	return this.each(function(index, elm){
+		$.data(elm, key, val);
+	});
+}
+
+$.fn.getData = function(key){
+	return $.map(this.each(function(index, elm){
+		return $.data(elm, key);
+	}));
+}
+*/
+
+$.showContent = function(callback){
+	$.ajax({
+		url: $.my.prop.contentUrl,
+		dataType: "json",
+		cache: false,
+		success: function(data){
+			
+			var
+			$tablist = $($.my.slctr.tabList),
+			$bcRight = $($.my.slctr.pattrnContWrap),
+			itms = data.items;
+
+			for(var i = 0, j = itms.length; i < j; i++){
+				var title = itms[i].title;
+				$tablist.append('<li class="tab"><a href="javascript:void(0)">' + title.ja + ' - ' + title.en + '</a></li>');
+				
+				var
+				cnts = itms[i].content,
+				$cont = $('<div>').addClass('cont');
+				
+				for(var k = 0, m = cnts.length; k < m; k++){
+					$('<div>')
+						.addClass('pt-wrap')
+						.html('<a href="javascript:void(0)" class="pattern">' + cnts[k].pt.$t + '</a>')
+						.appendTo($cont);
+					
+					$('<dl>')
+						.html(
+							(function(dt){ return dt ? '<dt>' + dt + '</dt>' : '' })(cnts[k].dl.dt) +
+							(function(dd){ return dd ? '<dd>' + dd.replace(/\n/g,'<br>') + '</dd>' : '' })(cnts[k].dl.dd)
+						).appendTo($cont);
+				}
+				
+				$bcRight.append($cont);
+			}
+			
+			// 調整
+			$('#tab-list li.tab').eq(5).css('margin-bottom', '20px');
+			
+			$('.cont:last').find('div.pt-wrap a.pattern').each(function(index, elm){
+				var $t = $(elm);
+				$t.before($('<span>')
+					.addClass($t.attr('class'))
+					.text($t.text())).remove();
+			});
+			// 調整 end
+			
+			if($.isFunction(callback)){callback.call()}
+		}
+	});
+}
+
+$.accordion = function( options ) {
+	var opt = jQuery.extend({
+		tglSelector:$.my.slctr.tabListTgl,
+		contSelector:$.my.slctr.pattrnCont,
+		duration:250
+	},options),
+	$conts = $(opt.contSelector),
+	$tgls = $(opt.tglSelector);
+	
+	$conts.hide().filter(':first').show();
+	$tgls.eq(0).addClass('current');
+
+	$tgls.click( function(e) {
+		var
+		$t = $(e.target),
+		$c = $conts.eq($tgls.index($t));
+		
+		if($c.is(':hidden')){
+			$conts.filter(':visible').slideUp(opt.duration);
+			$tgls.filter('.current').removeClass('current');
+			$c.slideDown(opt.duration);
+			$t.addClass('current');
+			$t.blur();
+		}
+		return false;
+	});
+	
+	//高さの調整
+	$.arrangeHeight();
+}
+
+$.arrangeHeight = function(){
+	var tablstHeight = $($.my.slctr.tabList).height();
+	$($.my.slctr.pattrnCont).each(function(){
+		if( $(this).height() < tablstHeight ){
+			$(this).height(tablstHeight);
+		}
+	});
+}
+
+$.setTitle = function(){
+	var pagetitle = $.my.prop.title.en + ' ver. ' + $.my.prop.ver;
+	$($.my.slctr.title).text(pagetitle);
+	$($.my.slctr.fTitle).text(pagetitle);
+}
+
+$.setText = function( value ) {
+	$($.my.slctr.tgt).val(value);
+}
+
+$.showLastModDate = function(){
+	$($.my.slctr.lastmod).text((function(date){
+		return (date.getFullYear() + 
+			$.my.prop.dateDelimiter + 
+			(date.getMonth() + 1) + 
+			$.my.prop.dateDelimiter + 
+			date.getDate());
+	})(new Date(document.lastModified)));
+}
+
+$.setLabelEffect = function(){
+	$($.my.slctr.lbl)
+		.css('color', '#999')
+		.hover(
+			function(){$(this).stop(true).animate({color:'#fff'}, 250)},
+			function(){$(this).stop(true).animate({color:'#999'}, 400)}
+		);
+}
+
+$.bindExec = function(){
+	var
+	attrs = ['backgroundColor'],
+	def_attrs = {},
+	$exe = $($.my.slctr.execBtn);
+	
+	for(var i = 0; i < attrs;i++){
+		def_attrs[attrs[i]] = $('#exec').css(attrs[i]);
+	}
+	
+	$('#exec')
+		.hover(function(){
+			$(this).stop(true).flash(attrs);
+		}, function(){
+			$(this).css(def_attrs);
+		})
+		.click(function(){
+			$.rpc.fnc();
+		});
+}
+
+$.fn.flash = function(attrs, options){
+	var opt = $.extend({
+		duration: 400,
+		easing: 'swing',
+		complete: function(){}
+	},options);
+	
+	attrs = attrs || ['backgroundColor'];
+	
+	return this.each(function(){
+		var def_attrs = {}, hi_attrs = {};
+		
+		if($.data(this, 'flash')){
+			def_attrs = $.data(this, 'flash').def_attrs;
+			hi_attrs = $.data(this, 'flash').hi_attrs;
+		}else{
+			for (var i = 0; i < attrs.length;i++) {
+				def_attrs[attrs[i]] = $(this).css(attrs[i]);
+				hi_attrs[attrs[i]] = $.modifyBrightness(def_attrs[attrs[i]], '+=20');
+			}
+			$.data(this, 'flash',{'def_attrs': def_attrs, 'hi_attrs': hi_attrs});
+		}
+		
+		$(this).css(hi_attrs).animate(def_attrs, opt);
+	});
+}
+
+
+$.bindPastePattern = function(){
+	$($.my.slctr.pattern).live('click', function(){
+		$($.my.slctr.ptTxt).val($(this).text()).parent().flash();
+		$(this).blur();
+		return false;
+	});
+}
+
+})(jQuery);
