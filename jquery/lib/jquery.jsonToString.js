e4356058ec765a31a1f80e6e7eaf35f09ab943f5
ysnr.ws <ysnr.ws@3b15c62c-4380-11de-9805-9778bc541ee5>
2009-06-06 03:15:25 +0000

    update jsonToString

---
+++ jquery/lib/jquery.jsonToString.js
@@ -0,0 +1,53 @@
+/*
+ * jQuery plugin jsonToString
+ * Copyright 2009
+ * Author y@s
+ * Updated 2009-06-06
+ * Version 1.0
+ * Released under the MIT and GPL licenses.
+ * 
+ * demo http://humming-bird.googlecode.com/svn/trunk/jquery/demo/jsonToString.html
+ */
+
+function escapeAndModify(s){
+	return (s || '').replace(/\\/g, '\\\\').replace(/\n/g, '\\n').replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/'/g, '&apos;').replace(/"/g, '&quot;');
+}
+
+
+(function($){
+$.jsonToString = function(json){
+
+	function oFn(o){
+		
+		if (o === null) return 'null';
+		if (o === undefined) return 'undefined';
+		
+		switch (o.constructor) {
+			case Boolean:
+			case Number:
+				return o;
+			case String:
+				return '\'' + o + '\'';
+			case Date:
+				return '\'' + o.toString() + '\'';
+			case Array:
+				var arr = [];
+				for (var i = 0, k = o.length; i < k; i++)
+					arr[arr.length] = oFn(o[i]);
+				
+				return '[' + arr.join(',') + ']';
+			case Object:
+					var arr = [];
+					for (var x in o) {
+						if (o.hasOwnProperty(x))
+							arr.push(x + ':' + oFn(o[x]));
+					}
+					return '{' + arr.join(',') + '}';
+			default:
+				return null;
+		}
+	}
+	
+	return oFn(json);
+}
+})(jQuery);
