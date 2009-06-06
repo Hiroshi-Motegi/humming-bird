203245a51475917408046f164e0b41cb82bc07d0
ysnr.ws <ysnr.ws@3b15c62c-4380-11de-9805-9778bc541ee5>
2009-06-06 18:14:43 +0000

    update jQuery plugin - flash

--- jquery/lib/jquery.flash.js
+++ jquery/lib/jquery.flash.js
@@ -1,26 +1,35 @@
 /*
  * jQuery plugin Flash
  * Copyright 2009 y@s
+ * Version 1.1
  * Released under the MIT and GPL licenses.
- 
- * Dependencies:。
+ * 
+ * Dependencies:
  * [ jQuery plugin changeBright ]
- *  
+ * 
+ * @Param bright : '+=20', '40', '60%' など
+ * ＠Param attr : 'backgroundColor', 'borderColor', 'color'
+ * @Param options : animate options
+ * 
  * flash 初回コール時にデフォルトの色を記憶する仕様。
  * 理由はこのエフェクトはコール時の色に戻す動作のため、続けて複数回呼ばれるとデフォルトの色が変わってしまうため。
  */
 
 (function($){
-$.fn.flash = function(bright, attrs, options){
+$.fn.flash = function(bright, attr, options){
 	
-	options = (!$.isArray(bright) && typeof bright == 'object') ? bright :
-		(!$.isArray(attrs) && typeof attrs == 'object') ? attrs : options || {};
-
-	attrs = $.isArray(bright) ? bright : $.isArray(attrs) ? attrs : ['backgroundColor'];
+	function isAttr(attr){
+		var attrs = ['backgroundColor', 'borderColor', 'color'];
+		for (var i= 0; i < attrs.length ; i++)
+			if( attr == attrs[i] ) return true;
+		return false;
+	}
 	
-	bright = typeof bright == 'string' || +bright == bright ? bright : '+=40';
+	options = bright && bright.constructor == Object ? bright : attr && attr.constructor == Object ? attr : options || {};
+	attr = typeof bright === 'string' && isAttr(bright)? bright : attr && isAttr(attr) ? attr : 'backgroundColor';
+	bright = typeof bright === 'string' && /^[-+]?\d%?$/.test(bright) || +bright == bright ? bright : '+=50';
 
-	var opt = $.extend({
+	var op = $.extend({
 		duration: 400,
 		easing: 'swing',
 		complete: function(){}
@@ -29,36 +38,38 @@ $.fn.flash = function(bright, attrs, options){
 	
 	return this.each(function(){
 		
-		var $t = $(this);
-
-		if(!$.data(this, dataKey)){
+		function toArray(attr){
+			if (attr == 'borderColor') 
+				return ['borderTopColor', 'borderBottomColor', 'borderLeftColor', 'borderRightColor'];
+			
+			return [attr];
+		}
+		
+		var $t = $(this), attrs = toArray(attr);
+		
+		if(!$.data(this, dataKey) || !$.data(this, dataKey)[attrs[0]]){
 			
 			var tmp = {};
 			
-			$.each([
-				'backgroundColor',
-				'borderBottomColor',
-				'borderLeftColor',
-				'borderRightColor',
-				'borderTopColor',
-				'color'],
-				 function(i, attr){
-				 	var c = $t.css(attr);
-					tmp[attr] = c == 'transparent' ? '#000' : c;
-				});
+			for(var i = 0, j = attrs.length; i < j; i++ ){
+				var c = $t.css(attrs[i]);
+				tmp[attrs[i]] = c == 'transparent' ? '#000' : c;
+			}
 			
-			$.data(this, dataKey,{'def_attrs': tmp});
+			$.data(this, dataKey, tmp);
+			
 		}
 		
 		var def_attrs = {}, hi_attrs = {};
 		
 		for (var i = 0, j = attrs.length; i < j; i++) {
 			var key = attrs[i];
-			def_attrs[key] = $.data(this, dataKey).def_attrs[key];
+			
+			def_attrs[key] = $.data(this, dataKey)[key];
 			hi_attrs[key] = $.changeBright(def_attrs[key], bright);
 		}
 		
-		$t.css(hi_attrs).animate(def_attrs, opt);
+		$t.css(hi_attrs).animate(def_attrs, op);
 		
 	});
 }
